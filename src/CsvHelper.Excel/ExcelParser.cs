0f1c6cf60bee76c06572bdcc3a9bea0c4844be40
Chris Rodgers <chris.rodgers@existec.com>
2015-03-10 15:05:05 +0000

    Initial Commit

---
+++ src/CsvHelper.Excel/ExcelParser.cs
@@ -0,0 +1,177 @@
+﻿
+namespace CsvHelper.Excel
+{
+    using System;
+    using System.Linq;
+    using ClosedXML.Excel;
+    using CsvHelper.Configuration;
+
+    /// <summary>
+    /// Parses an Excel file.
+    /// </summary>
+    public class ExcelParser : ICsvParser
+    {
+        private readonly XLWorkbook workbook;
+        private readonly bool disposeWorkbook;
+        private readonly IXLWorksheet worksheet;
+        private readonly CsvConfiguration configuration;
+        private bool disposed;
+        private int currentRow = 1;
+
+        /// <summary>
+        /// Creates a new parser using a new <see cref="XLWorkbook"/> from the given <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        public ExcelParser(string path) : this(path, new CsvConfiguration()) { }
+
+        /// <summary>
+        /// Creates a new parser using a new <see cref="XLWorkbook"/> from the given <paramref name="path"/> and uses the given <paramref name="configuration"/>.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="configuration">The configuration.</param>
+        public ExcelParser(string path, CsvConfiguration configuration)
+            : this(new XLWorkbook(path, XLEventTracking.Disabled), configuration)
+        {
+            disposeWorkbook = true;
+        }
+
+        /// <summary>
+        /// Creates a new parser using the given <see cref="XLWorkbook"/>.
+        /// <remarks>
+        /// Will attempt to read the data from the first worksheet in the workbook.
+        /// </remarks>
+        /// </summary>
+        /// <param name="workbook">The <see cref="XLWorkbook"/> with the data.</param>
+        public ExcelParser(XLWorkbook workbook) : this(workbook, new CsvConfiguration()) { }
+
+        /// <summary>
+        /// Creates a new parser using the given <see cref="XLWorkbook"/> and <see cref="CsvConfiguration"/>.
+        /// <remarks>
+        /// Will attempt to read the data from the first worksheet in the workbook.
+        /// </remarks>
+        /// </summary>
+        /// <param name="workbook">The <see cref="XLWorkbook"/> with the data.</param>
+        /// <param name="configuration">The configuration.</param>
+        public ExcelParser(XLWorkbook workbook, CsvConfiguration configuration) : this(workbook.Worksheets.First(), configuration) { }
+
+        /// <summary>
+        /// Creates a new parser using the given <see cref="IXLWorksheet"/>.
+        /// </summary>
+        /// <param name="worksheet">The <see cref="IXLWorksheet"/> with the data.</param>
+        public ExcelParser(IXLWorksheet worksheet) : this(worksheet, new CsvConfiguration()) { }
+
+        /// <summary>
+        /// Creates a new parser using the given <see cref="IXLWorksheet"/> and <see cref="CsvConfiguration"/>.
+        /// </summary>
+        /// <param name="worksheet">The <see cref="IXLWorksheet"/> with the data.</param>
+        /// <param name="configuration">The configuration.</param>
+        public ExcelParser(IXLWorksheet worksheet, CsvConfiguration configuration)
+        {
+            workbook = worksheet.Workbook;
+            this.worksheet = worksheet;
+            this.configuration = configuration;
+            FieldCount = worksheet.RowsUsed().CellsUsed().Max(cell => cell.Address.ColumnNumber);
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        public CsvConfiguration Configuration { get { return configuration; } }
+
+        /// <summary>
+        /// Gets the workbook from which we are reading data.
+        /// </summary>
+        /// <value>
+        /// The workbook.
+        /// </value>
+        public XLWorkbook Workbook { get { return workbook; } }
+
+        /// <summary>
+        /// Gets the field count.
+        /// </summary>
+        public int FieldCount { get; private set; }
+
+        /// <summary>
+        /// Gets the character position that the parser is currently on.
+        /// <remarks>This feature is unused.</remarks>
+        /// </summary>
+        public long CharPosition { get { return -1; } }
+
+        /// <summary>
+        /// Gets the byte position that the parser is currently on.
+        /// <remarks>This feature is unused.</remarks>
+        /// </summary>
+        public long BytePosition { get { return -1; } }
+
+        /// <summary>
+        /// Gets the row of the Excel file that the parser is currently on.
+        /// </summary>
+        public int Row { get { return currentRow; } }
+
+        /// <summary>
+        /// Gets the raw row for the current record that was parsed.
+        /// </summary>
+        public virtual string RawRecord { get { return worksheet.Row(currentRow).Cells(1, FieldCount).ToString(); } }
+
+        /// <summary>
+        /// Reads a record from the Excel file.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:String[]" /> of fields for the record read.
+        /// </returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the parser has been disposed.</exception>
+        public virtual string[] Read()
+        {
+            CheckDisposed();
+            var result = worksheet.Row(currentRow)
+                .Cells(1, FieldCount)
+                .Select(cell => cell.Value.ToString())
+                .ToArray();
+            currentRow++;
+            return result;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="ExcelParser"/> class.
+        /// </summary>
+        ~ExcelParser()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                if (disposeWorkbook) workbook.Dispose();
+            }
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Checks if the instance has been disposed of.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException" />
+        protected virtual void CheckDisposed()
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(GetType().ToString());
+            }
+        }
+    }
+}
---
+++ src/CsvHelper.Excel/ExcelSerializer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace CsvHelper.Excel
+{
+    using System;
+    using ClosedXML.Excel;
+    using CsvHelper.Configuration;
+
+    /// <summary>
+    /// Defines methods used to serialize data into an Excel (2007+) file.
+    /// </summary>
+    public class ExcelSerializer : ICsvSerializer
+    {
+        private readonly string path;
+        private readonly XLWorkbook workbook;
+        private readonly bool disposeWorkbook;
+        private readonly IXLWorksheet worksheet;
+        private readonly bool disposeWorksheet;
+        private readonly CsvConfiguration configuration;
+        private bool disposed;
+        private int currentRow = 1;
+
+        /// <summary>
+        /// Creates a new serializer using a new <see cref="XLWorkbook"/> saved to the given <paramref name="path"/>.
+        /// <remarks>
+        /// The workbook will not be saved until the serializer is disposed.
+        /// </remarks>
+        /// </summary>
+        /// <param name="path">The path to which to save the workbook.</param>
+        public ExcelSerializer(string path) : this(new XLWorkbook(XLEventTracking.Disabled))
+        {
+            this.path = path;
+            disposeWorkbook = true;
+        }
+
+        /// <summary>
+        /// Creates a new serializer using the given <see cref="XLWorkbook"/>.
+        /// <remarks>
+        /// The <paramref name="workbook"/> will <b><i>not</i></b> be disposed of when the serializer is disposed.
+        /// The workbook will <b><i>not</i></b> be saved by the serializer.
+        /// A new worksheet will be added to the workbook.
+        /// </remarks>
+        /// </summary>
+        /// <param name="workbook">The workbook to write the data to.</param>
+        public ExcelSerializer(XLWorkbook workbook) : this(workbook, new CsvConfiguration()) { }
+
+        /// <summary>
+        /// Creates a new serializer using the given <see cref="XLWorkbook"/> and <see cref="CsvConfiguration"/>.
+        /// <remarks>
+        /// The <paramref name="workbook"/> will <b><i>not</i></b> be disposed of when the serializer is disposed.
+        /// The workbook will <b><i>not</i></b> be saved by the serializer.
+        /// A new worksheet will be added to the workbook.
+        /// </remarks>
+        /// </summary>
+        /// <param name="workbook">The workbook to write the data to.</param>
+        /// <param name="configuration">The configuration.</param>
+        public ExcelSerializer(XLWorkbook workbook, CsvConfiguration configuration)
+            : this(workbook.AddWorksheet(String.Empty), configuration)
+        {
+            disposeWorksheet = true;
+        }
+
+        /// <summary>
+        /// Creates a new serializer using the given <see cref="IXLWorksheet"/>.
+        /// <remarks>
+        /// The <paramref name="worksheet"/> will <b><i>not</i></b> be disposed of when the serializer is disposed.
+        /// The workbook will <b><i>not</i></b> be saved by the serializer.
+        /// </remarks>
+        /// </summary>
+        /// <param name="worksheet">The worksheet to write the data to.</param>
+        public ExcelSerializer(IXLWorksheet worksheet) : this(worksheet, new CsvConfiguration()) { }
+
+        /// <summary>
+        /// Creates a new serializer using the given <see cref="IXLWorksheet"/>.
+        /// <remarks>
+        /// The <paramref name="worksheet"/> will <b><i>not</i></b> be disposed of when the serializer is disposed.
+        /// The workbook will <b><i>not</i></b> be saved by the serializer.
+        /// </remarks>
+        /// </summary>
+        /// <param name="worksheet">The worksheet to write the data to.</param>
+        /// <param name="configuration">The configuration</param>
+        public ExcelSerializer(IXLWorksheet worksheet, CsvConfiguration configuration)
+        {
+            workbook = worksheet.Workbook;
+            this.worksheet = worksheet;
+            this.configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        public CsvConfiguration Configuration { get { return configuration; } }
+
+        /// <summary>
+        /// Gets the workbook to which the data is being written.
+        /// </summary>
+        /// <value>
+        /// The workbook.
+        /// </value>
+        public XLWorkbook Workbook { get { return workbook; } }
+
+        /// <summary>
+        /// Writes a record to the Excel file.
+        /// </summary>
+        /// <param name="record">The record to write.</param>
+        /// <exception cref="ObjectDisposedException">
+        /// Thrown is the serializer has been disposed.
+        /// </exception>
+        public virtual void Write(string[] record)
+        {
+            CheckDisposed();
+            for (var i = 0; i < record.Length; i++)
+            {
+                worksheet.Cell(currentRow, i).Value = record[i];
+            }
+            currentRow++;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="ExcelSerializer"/> class.
+        /// </summary>
+        ~ExcelSerializer()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                if (disposeWorksheet) worksheet.Dispose();
+                if (disposeWorkbook)
+                {
+                    workbook.SaveAs(path);
+                    workbook.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Checks if the instance has been disposed of.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">
+        /// Thrown is the serializer has been disposed.
+        /// </exception>
+        protected virtual void CheckDisposed()
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(GetType().ToString());
+            }
+        }
+    }
+}
